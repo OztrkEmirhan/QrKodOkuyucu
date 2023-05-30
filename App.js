ad7e5cf111b20c85422c3bb02e7b945e73f90868
OztrkEmirhan <107921700+OztrkEmirhan@users.noreply.github.com>
2023-05-30 16:03:22 +0300

    publish
    publish

---
+++ App.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button, Linking, TouchableOpacity } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState('Qr Code Scanner')
+
+  const askForCameraPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })()
+  }
+
+  // Request Camera Permission
+  useEffect(() => {
+    askForCameraPermission();
+  }, []);
+
+  // What happens when we scan the bar code
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data)
+    console.log('Type: ' + type + '\nData: ' + data)
+  };
+
+  // Check permissions and return the screens
+  if (hasPermission === null) {
+    return (
+      <View style={styles.container}>
+        <Text>Requesting for camera permission</Text>
+      </View>)
+  }
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ margin: 10 }}>No access to camera</Text>
+        <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
+      </View>)
+  }
+
+  // Return the View
+  return (
+    <View style={styles.container}>
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 400, width: 400 }} />
+      </View>
+
+
+    <View style={{width:"auto", height:"auto", borderRadius:50, backgroundColor:"yellow", margin:20}}>
+      <Text style={styles.maintext}>{text}</Text>
+    </View>
+
+    <View style={{flexDirection:"row",}}>
+        {scanned && <Button title={'Tekrar Dene'} onPress={() => setScanned(false)} color='tomato' />}
+        <View style={{marginLeft:10}}>
+        {scanned && <Button title={"Link'e Git"} onPress={() => Linking.openURL(text)} color='green' />}
+        </View>
+    </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  maintext: {
+    fontSize: 16,
+    margin: 20,
+    fontWeight:"bold"
+  },
+  barcodebox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 300,
+    width: 300,
+    overflow: 'hidden',
+    borderRadius: 30,
+    backgroundColor: 'tomato'
+  }
+});
